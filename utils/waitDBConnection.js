f6be506082b33f5aa6cc96f2c328d3a0bfdca053
huzaifa321s <muhammadhuzaifa@humantekteam.com>
2025-12-25 16:17:50 +0500

    Add mongoose import to waitForDbConnection

--- utils/waitDBConnection.js
+++ utils/waitDBConnection.js
@@ -1,7 +1,7 @@
 const waitForDbConnection = (timeout = 5000, interval = 100) => {
     return new Promise((resolve, reject) => {
         const startTime = Date.now();
-
+        const mongoose = require('mongoose');
         const check = () => {
             if (mongoose.connection.readyState === 1) {
                 return resolve(true);
